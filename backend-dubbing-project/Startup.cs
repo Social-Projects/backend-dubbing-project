a14c6416bab196c23196adc7f58ba0e360fd7088
Mikhail Zmievskiy <mishazmievskiy@gmail.com>
2018-12-29 22:28:38 +0200

    Solve the issue

--- backend-dubbing-project/Startup.cs
+++ backend-dubbing-project/Startup.cs
@@ -23,7 +23,6 @@ namespace backend_dubbing_project
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -34,7 +33,6 @@ namespace backend_dubbing_project
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -42,8 +40,7 @@ namespace backend_dubbing_project
                 app.UseDeveloperExceptionPage();
             }
             else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            {                
                 app.UseHsts();
             }
             
