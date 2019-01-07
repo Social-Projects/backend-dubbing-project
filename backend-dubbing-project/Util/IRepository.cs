52f0bfdb2be9625e1b686abc831579a28ffeb8b8
Illya Kostiuchyk <kosstill28@gmail.com>
2019-01-07 13:00:56 +0200

    Create generic repository

---
+++ backend-dubbing-project/Util/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_dubbing_project.Util
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAllItems();
+        T GetItem(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
