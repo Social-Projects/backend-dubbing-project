c4126d0cdc77b3e0941e5bd2fc3bb458a1f697a1
Andrei Kasyanchuk <kasanandry@gmail.com>
2019-02-19 21:28:52 +0200

    Refactored language controller

--- Web/ApiControllers/LanguageController.cs
+++ Web/ApiControllers/LanguageController.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SoftServe.ITAcademy.BackendDubbingProject.Administration.Core.Entities;
 using SoftServe.ITAcademy.BackendDubbingProject.Administration.Core.Interfaces;
 
 namespace SoftServe.ITAcademy.BackendDubbingProject.Web.ApiControllers
@@ -12,6 +17,63 @@ namespace SoftServe.ITAcademy.BackendDubbingProject.Web.ApiControllers
         public LanguageController(ILanguageService languageService)
         {
             _languageService = languageService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Language>>> Get()
+        {
+            var listOfAllLanguages = await _languageService.GetAllLanguages();
+
+            if (listOfAllLanguages.Count() == 0)
+                return NotFound();
+
+            return Ok(listOfAllLanguages);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Language>> GetById(int id)
+        {
+            var language = await _languageService.GetById(id);
+
+            if (language == null)
+                return NotFound();
+
+            return Ok(language);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Language>> Create(Language model)
+        {
+            if (model == null)
+                return BadRequest();
+
+            await _languageService.Create(model);
+
+            return CreatedAtAction(nameof(GetById), new { id = model.Id }, model);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Language>> Delete(int id)
+        {
+            await _languageService.Delete(id);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Language>> Update(Language model)
+        {
+            await _languageService.Update(model);
+
+            return NoContent();
         }
     }
 }
