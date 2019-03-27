2fba0d365f7d141cea28879213d3b7e47a1bedb0
Mikhail Zmievskiy <mishazmievskiy@gmail.com>
2019-03-28 00:06:14 +0200

    Minor fix.

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -37,7 +37,7 @@ namespace SoftServe.ITAcademy.BackendDubbingProject.Web
             services.AddCors(options =>
             {
                 options.AddPolicy(
-                    "http://104.248.28.238:3000",
+                    CorsName,
                     builder =>
                     {
                         builder
